ec90e611b1bfaed1b34b57fad63970fdf5b5e6e9
Rory Magee <rory.magee2@gmail.com>
2019-01-06 00:28:55 +0000

    finished project page layout

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,13 +6,27 @@ import { Project } from '../projectClass';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-  
+  projString: string = '</Projects>'
   news: Project = {
     name: 'News Website',
-    description: 'Description sdjkfksjdfh kjsdkfj nksjdf kjsd fkjsdnfkjsndf knjsfjksf jknskdf nskjdfn ksjd nfksnjdfkjsndf kjsndfknjsdfkj nskdfj ns asjkd ajksdbkajsbd kjasb dkbjasdkjbasdkbjasdkjb kjb kdf nskjdf ksjd nfksjd nf knjsdf jknsdfknjsdf kjnsdf kjnsf knjsfkd jnskdf jnsdkf jnkj',
+    description: 'This news website makes HTTP requests to a news distribution API and pulls up to date articles which are displayed to the user. The website has a built in search feature and users can also filter stories using several categories.',
     image: '../assets/img/news.PNG',
-    git_link: 'placeholder',
-    direct_link: 'placeholder'
+    git_link: 'https://github.com/RoryMagee/News',
+    direct_link: 'http://news-server.eu-west-1.elasticbeanstalk.com/'
+  } 
+  eshop: Project = {
+    name: 'Ecommerce Website',
+    description: 'This website is an implementation of an ecommerce website which offers functionality such as account creation, fully functioning search, users are able to add products to their basket and checkout. Users are also able to register as a seller and upload their own products for sale.',
+    image: '../assets/img/shop.PNG',
+    git_link: 'https://github.com/RoryMagee/E-Shop',
+    direct_link: 'http://shop.snspbvwdfe.eu-west-1.elasticbeanstalk.com/#/'
+  }
+  weather: Project = {
+    name: 'Weather Website',
+    description: 'Local weather information is pulled from a weather forecast API and is displayed for the user in an easily consumable format.',
+    image: '../assets/img/weather.PNG',
+    git_link: 'https://github.com/RoryMagee/weather',
+    direct_link: 'http://weathersite-env.xrxtpebiqm.eu-west-1.elasticbeanstalk.com/#/'
   }
   constructor() { }
 
